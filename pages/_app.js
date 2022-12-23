6572f4485439073076268a80a2da973e106529ca
TitamSeptian <dwiputra4456@protonmail.com>
2022-12-24 05:45:59 +0700

    profile professional

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
 import Head from "next/head";
 import "../styles/globals.css";
-import {ChakraProvider, extendTheme} from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@fontsource/inter";
 import theme from "../styles/theme/theme";
 import theme2 from "../styles/theme";
 // import {router} from "next/client";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import DashboardLayout from "../components/dashboard/Layout";
 import MainLayout from "../components/landing/Layout";
-import UserLayout from  "../components/user/Layout"
+import UserLayout from "../components/user/Layout";
+import ProLayout from "../components/pro/Layout";
 import NextNProgress from "nextjs-progressbar";
 // import dynamic from "next/dynamic";
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
     const router = useRouter();
-    const {pathname} = router;
+    const { pathname } = router;
     const TheDefault = () => {
         return (
             <>
@@ -23,17 +24,23 @@ function MyApp({Component, pageProps}) {
                         name="viewport"
                         content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
                     />
-                    <link rel="icon" href="/favicon.ico"/>
+                    <link rel="icon" href="/favicon.ico" />
                     <title>Build it</title>
                 </Head>
-                <NextNProgress color="#EA6526" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true}/>
+                <NextNProgress
+                    color="#EA6526"
+                    startPosition={0.3}
+                    stopDelayMs={200}
+                    height={3}
+                    showOnShallow={true}
+                />
             </>
         );
     };
     if (pathname.startsWith("/dashboard")) {
         return (
             <>
-                <TheDefault/>
+                <TheDefault />
                 <ChakraProvider theme={theme}>
                     <DashboardLayout>
                         <Component {...pageProps} />
@@ -45,7 +52,7 @@ function MyApp({Component, pageProps}) {
     if (pathname.startsWith("/user")) {
         return (
             <>
-                <TheDefault/>
+                <TheDefault />
                 <ChakraProvider theme={theme}>
                     <UserLayout>
                         <Component {...pageProps} />
@@ -53,10 +60,22 @@ function MyApp({Component, pageProps}) {
                 </ChakraProvider>
             </>
         );
+    }
+    if (pathname.startsWith("/pro")) {
+        return (
+            <>
+                <TheDefault />
+                <ChakraProvider theme={theme}>
+                    <ProLayout>
+                        <Component {...pageProps} />
+                    </ProLayout>
+                </ChakraProvider>
+            </>
+        );
     } else {
         return (
             <>
-                <TheDefault/>
+                <TheDefault />
                 <ChakraProvider theme={theme}>
                     <MainLayout>
                         <Component {...pageProps} />
---
+++ pages/pro/project/index.js
@@ -0,0 +1,91 @@
+import {
+    VStack,
+    Heading,
+    Text,
+    HStack,
+    Button,
+    useDisclosure,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+export default function project() {
+    const projectSample = [
+        {
+            id: 1,
+            name: "Project 1",
+            project: "Jl. Raya Cibubur No. 1",
+        },
+        {
+            id: 2,
+            name: "Project 2",
+            project: "Jl. Raya Cibubur No. 2",
+        },
+        {
+            id: 3,
+            name: "Project 3",
+            project: "Jl. Raya Cibubur No. 3",
+        },
+    ];
+    const handleDelete = (id) => {
+        alert(id);
+        console.log(id);
+    };
+    return (
+        <>
+            <HStack
+                bg={"white"}
+                borderRadius="lg"
+                justifyContent={"space-between"}
+                py={"2"}
+                mt={2}
+                px={"4"}
+            >
+                <Heading size={"md"}>Log Project</Heading>
+                <Link href={"/pro/project/create"}>
+                    <Button
+                        variant={"outline"}
+                        size="sm"
+                        colorScheme="myorange"
+                    >
+                        Tambah Projects
+                    </Button>
+                </Link>
+            </HStack>
+            {projectSample.map((project) => (
+                <VStack
+                    key={project.id}
+                    bg={"white"}
+                    borderRadius={"lg"}
+                    py={"4"}
+                    mt={2}
+                    px={"4"}
+                    alignItems={"start"}
+                    // rowGap={"6"}
+                    columnGap={"10"}
+                >
+                    <Heading size={"md"}>{project.name}</Heading>
+                    <Text fontSize={"sm"}>{project.project}</Text>
+                    <HStack alignSelf={"end"}>
+                        <Link href={`/pro/project/${project.id}`}>
+                            <Button
+                                variant={"ghost"}
+                                size="sm"
+                                colorScheme={"blue"}
+                            >
+                                Edit
+                            </Button>
+                        </Link>
+                        <Button
+                            variant={"ghost"}
+                            size="sm"
+                            colorScheme={"red"}
+                            onClick={(e) => handleDelete(project.id)}
+                        >
+                            Hapus
+                        </Button>
+                    </HStack>
+                </VStack>
+            ))}
+        </>
+    );
+}
